2223938f655d46cba123bbac50af1d764b14c563
sanket-pilane <lalitpilane7@gmail.com>
2025-12-02 22:14:47 +0530

    feat: implemented TiffinBloc for state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tiffin_mate/core/theme/app_theme.dart';
+import 'package:tiffin_mate/data/repositories/tiffin_repository.dart';
+import 'package:tiffin_mate/data/repositories/tiffin_repository_impl.dart';
+import 'package:tiffin_mate/logic/blocs/bloc/tiffin_bloc.dart';
+import 'package:tiffin_mate/logic/blocs/bloc/tiffin_event.dart';
 import 'package:tiffin_mate/presentation/screens/dashboard_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const TiffinApp());
+  final tiffinRepository = TiffinRepositoryImpl();
+  await tiffinRepository.initialize();
+
+  runApp(TiffinApp(repository: tiffinRepository));
 }
 
 class TiffinApp extends StatelessWidget {
-  const TiffinApp({super.key});
+  final TiffinRepository repository;
+
+  const TiffinApp({super.key, required this.repository});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TiffinMate',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
-      home: const DashboardScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              TiffinBloc(repository: repository)..add(LoadTiffins()),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'TiffinMate',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: ThemeMode.system,
+        home: const DashboardScreen(),
+      ),
     );
   }
 }
